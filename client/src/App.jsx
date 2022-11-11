b303b6d635648f7c6301f649425c8116f456ef88
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2022-11-11 18:28:39 -0500

    Jueputa

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+        <h1>Hola
+
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat fugit alias non quaerat a, optio odio beatae ipsa nihil, quia qui incidunt atque magnam quisquam minima rem aliquam possimus. Repellendus?</p>
+        
+    </>
+  )
+}
+
+export default App
