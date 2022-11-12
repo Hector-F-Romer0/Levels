bca0d8a7951c09a076f34c92a0926ea11dee898d
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2022-11-11 22:08:13 -0500

    Gonorrea

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
-    <>
-       
-        <h1>Hola
-
-        </h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat fugit alias non quaerat a, optio odio beatae ipsa nihil, quia qui incidunt atque magnam quisquam minima rem aliquam possimus. Repellendus?</p>
-        
-    </>
+    <Navbar/>
+  
   )
 }
 
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header> 
+          <h1>Levels</h1>
+            <nav>  
+              <li><a href='#'>Inicio</a></li>
+              <li><a href='#'>Usuario</a></li>
+            </nav>  
+        </header>
+  )
+}
+
+export default Navbar
